04ebbfe4b302df5cc73549e7a13a55f4800ca0fe
Joris Snellenburg <j.snellenburg@vu.nl>
2016-01-29 15:19:15 +0100

    Added variable projection example in R code

--- tests/kin_sim_simple_script.R
+++ tests/kin_sim_simple_script.R
@@ -21,6 +21,7 @@ E[, 3] <- amp[3] * exp( - log(2) * (2 * (wavenum - location[3])/delta[3])^2)
 
 PSI <- matrix(nrow=length(times), ncol = length(wavenum))
 
+kinpar <- c(.005,.05,0.09)
 
 for (i in 1:length(wavenum)) {
 #     irfvec <- irfparF(irfpar = c(57.47680283, 1.9), lambdac = 1500, 
@@ -31,7 +32,7 @@ for (i in 1:length(wavenum)) {
 #     wavenum[i], i=1, mudisp = TRUE, parmu = c(.0001,.0001), taudisp = FALSE,
 #     dispmufun = "poly")
 
-    C <- compModel (k=c(.005,.05,0.09), x=times, irfpar =irfvec, # cohirf = cohirf, 
+    C <- compModel (k=kinpar, x=times, irfpar =irfvec, # cohirf = cohirf, 
     irf = irf, # cohspec = list(type = "freeirfdisp"),coh = vector(), 
     lamb = i, dataset = 1,usekin2=FALSE) 
     
@@ -41,6 +42,32 @@ for (i in 1:length(wavenum)) {
 sigma <- .01
 PSI  <- PSI + sigma * rnorm(dim(C)[1] * dim(E)[1])
 
+# First use variable projection to calculate the conditional linear 
+# parameters (clp) from C and PSI. Here ET stand for estimated E^T.
+ET <- matrix(nrow=length(kinpar), ncol = length(wavenum))
+    # for each dimension that is not time (in this case just wavelength)
+    for (i in 1:dim(PSI)[2]) {
+        ET[,i] <- qr.coef(qr(C), PSI[,i])
+    }
+# 
+clp = t(ET)
+dev.new()
+matplot(clp)
+# obviously in R this can be done more efficiently for all wavelengths simultanously,
+# but this will only work if all the dimension are the same:
+# The spectra
+dev.new()
+clpplot <- matplot(t(qr.coef(qr(C), PSI)))
+# The fitted data (length(times)) number of spectra
+dev.new()
+matplot(t(qr.fitted(qr(C), PSI)))
+# The residuals at each timepoint
+dev.new()
+matplot(t(qr.resid(qr(C), PSI)))
+
+
+# This commented dfunction writes out the simulated data, with a bogus number appended to each lin
+# so that this can easily be replace by a space in order to translate it into a 1D vector.
 # write.table(rbind(c(c(0),wavenum,c(1234321234)),cbind(times,PSI,rep(1234321234,length(times)))),
 # file='3expdata.csv',row.names = FALSE,col.names=FALSE,sep=',')
 # sed -i 's:1234321234::g' 3expdata.csv
@@ -60,3 +87,9 @@ makeps="Sergey data", title="Ser")
 
 serRes<-fitModel(list(ser2), list(model1), 
 opt=kinopt(iter=11, plot = TRUE))
+
+# compare the results with the varpro we did earlier
+dev.new()
+clp2 <- getCLP(serRes)
+matplot(cbind(clp,clp2))
+

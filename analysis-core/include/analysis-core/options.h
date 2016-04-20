7c9f3d631705867386fe5f4aa4b50cd46c12106c
Stefan Schütz <yaminokeshin@gmail.com>
2016-04-20 20:35:35 +0200

    Added missing new Options class

---
+++ analysis-core/include/analysis-core/options.h
@@ -0,0 +1,43 @@
+#ifndef OPTIONS_H
+#define OPTIONS_H
+
+#include "analysis-core/internal/member.h"
+#include "analysis-core/internal/base.h"
+
+using namespace AnaylsisCore::internal;
+
+namespace AnalysisCore{
+   
+  class ANALYSIS_CORE_EXPORT Options : public internal::Base{
+  
+  public:
+    Options();
+    virtual ~Options();
+  };
+  
+}
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+  
+  ANALYSIS_CORE_EXPORT bool ac_options_get_bool(void* options, char* member_name);
+  ANALYSIS_CORE_EXPORT double ac_options_get_double(void* options, char* member_name);
+  ANALYSIS_CORE_EXPORT int ac_options_get_int(void* options, char* member_name);
+  ANALYSIS_CORE_EXPORT char* ac_options_get_string(void* options, char* member_name);
+  ANALYSIS_CORE_EXPORT double* ac_options_get_vector(void* options, char* member_name, int* num_rows);
+  ANALYSIS_CORE_EXPORT double* ac_options_get_matrix(void* options, char* member_name, int* num_rows, int* num_cols);
+  
+  ANALYSIS_CORE_EXPORT void ac_options_set_bool(void* options, char* member_name, bool value);
+  ANALYSIS_CORE_EXPORT void ac_options_set_double(void* options, char* member_name, double value);
+  ANALYSIS_CORE_EXPORT void ac_options_set_int(void* options, char* member_name, int value);
+  ANALYSIS_CORE_EXPORT void ac_options_set_string(void* options, char* member_name, const char* value);
+  ANALYSIS_CORE_EXPORT void ac_options_set_vector(void* options, char* member_name, double* vector, int num_rows);
+  ANALYSIS_CORE_EXPORT void ac_options_set_matrix(void* options, char* member_name, double* matrix, int num_rows, int num_cols);
+
+    
+#ifdef __cplusplus
+}
+#endif
+
+#endif // OPTIONS_H
